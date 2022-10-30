0ac49675c5d3802ebde34518132c60a06ca95215
Andrew Kang <yanjunk@students.cs.ubc.ca>
2022-10-29 19:28:01 -0700

    sre- try adding some general c

--- projects/sre/catalan_matrices.py
+++ projects/sre/catalan_matrices.py
@@ -1,4 +1,5 @@
 import math
+from abc import ABC, abstractmethod
 
 def print_matrix(matrix):
     print(''.join('-' * 8 for i in matrix))
@@ -8,7 +9,7 @@ def print_matrix(matrix):
 
 max_cat = 100
 
-catalans = [math.comb(2*i, i) // (i+1) for i in range(1, max_cat)]
+catalans = [math.comb(2*i, i) // (i+1) for i in range(max_cat)]
 
 def catalan_matrix(size):
     if 2 * size > max_cat + 1:
@@ -52,48 +53,96 @@ def reduce(size):
         print_matrix(mat)
     return mat
 
+class Variable:
+    def __init__(self, var):
+        self.var = var
+
+    def __mul__(self, var):
+        var = var.var
+        for k, v in var.items():
+            if k not in self.var:
+                self.var[k] = 0
+            self.var[k] += v
+        return self
+
+    def __eq__(self, var):
+        return self.var == var.var
+
+    def __str__(self):
+        return "".join([f"({{{k}}})^{{{v}}}" for k, v in self.var.items()])
+
+    def clone(self):
+        return Variable(self.var.copy())
+
 class ApCoeff:
-    def __init__(self, power, coeff):
+    def __init__(self, power, coeff, var):
         self.power = power
         self.coeff = coeff
+        self.var = var
 
-    def eval(self):
-        return catalans[self.power // 2] * self.coeff
+    def __str__(self):
+        if self.power % 2 == 1:
+            return '0'
+        return f"({catalans[self.power // 2] * self.coeff}){str(self.var)}"
 
-    def mult(self, ap):
-        return ApCoeff(self.power + ap.power, self.coeff * ap.coeff)
+    def __mul__(self, ap):
+        return ApCoeff(self.power + ap.power, self.coeff * ap.coeff, self.var.clone() * ap.var.clone())
+
+    def can_add(self, ap):
+        return self.var == ap.var and self.power == ap.power
+
+    def __add__(self, ap):
+        if not self.can_add(ap):
+            raise Exception('youre stupid')
+        self.coeff += ap.coeff
+        return self
+
+    def clone(self):
+        return ApCoeff(self.power, self.coeff, self.var.clone())
 
 class Equation:
     def __init__(self, apcoeffs):
         self.aps = apcoeffs
 
-    def mult(self, aps):
-        self.aps = [f.mult(s) for f in self.aps for s in aps]
+    def __mul__(self, eq):
+        self.aps = [f * s for f in self.aps for s in eq.aps]
         self.simplify_like()
+        return self.clone()
 
     def simplify_like(self):
-        d = {}
+        d = []
         for ap in self.aps:
-            if ap.power in d:
-                d[ap.power].coeff += ap.coeff
+            for elem in d:
+                if elem.can_add(ap):
+                    elem += ap
+                    break
             else:
-                d[ap.power] = ap
-        self.aps = list(d.values())
+                d.append(ap)
+        self.aps = d
 
-    def eval(self):
-        return abs(sum([ap.eval() for ap in self.aps]))
+    def __add__(self, eq):
+        self.aps.extend(eq.aps)
+        return self.clone()
+
+    def __str__(self):
+        return "+".join([str(ap) for ap in self.aps if str(ap) != '0'])
+
+    def clone(self):
+        return Equation([a.clone() for a in self.aps])
+
+    def __repr__(self):
+        return self.__str__()
+
+def second_degree():
+    a = Variable({'a': 1})
+    b = Variable({'b': 1})
+    c = Variable({'c': 1})
+    x = ApCoeff(1, 1, Variable({}))
+
+    t = Equation([x.clone(), ApCoeff(0, -1, c.clone())])
+    g = t.clone() * t.clone() * t.clone() + Equation([ApCoeff(0, 1, a.clone())]) * t.clone() * t.clone() + Equation([ApCoeff(0, 1, b.clone())]) * t.clone()
+    l = g.clone() * g.clone()
+    print(f"\\frac{{({g})^2}}{{{l}}}")
 
 
-def holders_inequality_main(max_w):
-    mult_eq = [ApCoeff(2, -4), ApCoeff(4, 1)]
-    eq = Equation([ApCoeff(2, -4), ApCoeff(4, 1)])
-    left_side = 2
-    a = "["
-
-    for i in range(1, max_w):
-        a += f"({i + 1}, \\sqrt[{i}]{{\\frac{{{left_side}}}{{{eq.eval()}}}}}),"
-        left_side *= 2
-        eq.mult(mult_eq)
-
-    return a + "]"
 
