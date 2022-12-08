83621b76750cc04ee148195778c119f96472f228
Andrew Kang <andrewkang.ubc2020@gmail.com>
2022-12-07 20:59:12 -0800

    PARSE - add some parse with inspiration from AOC

--- projects/parse.py
+++ projects/parse.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+
 from dataclasses import dataclass
 
 """[[(%[[{g_x_y^s}%]|   ):x,sep: ]\n:y]
@@ -8,41 +9,74 @@ from dataclasses import dataclass
 
 @dataclass
 class Context:
-    cur_vars: Dict
+    cur_vars: 
 
-class BaseNode(ABC):
-    @abstractmethod
-    def __init__(self, context):
+class BaseMatch(ABC):
+    def with_parent(self, parent):
+        self.parent = parent
+        return self
+
+    def with_context(self, context):
         self.context = context
+        return self
 
-    def create(self, template):
-        if fits_template
-        
-    """Does the current class even fit the template? eg. iteratingnode will match a '['.
-    It does not try to parse the entire template or syntax checks. Just checks the beginning essentials."""
-    @abstractmethod
-    def fits_template(self, template):
-        pass
-
-    """Syntax checks all of the valid possibilities"""
     @abstractmethod
     def fits_counts(self, test):
         return []
 
-    def getVarsFor(self, restStr):
-        partOfStr = restStr[0:self.fitsUpTo(restStr)]
+    def get_vars_for(self, rest_str):
+        partOfStr = rest_str[0:self.fitsUpTo(restStr)]
         return self.getMatchingDict(partOfStr)
 
     @abstractmethod
     def getMatchingDict(self, string):
         pass
 
+def node_factory(rest_template):
+    # try one.
+
+
+
+class ConcatMatch(BaseMatch):
+    def __init__(self, name):
+        self.children = {}
+        self.name = name
+
+    def add_child(self, name, child):
+        if name not in self.children:
+            self.children[name] = child.with_parent(self)
+            return True
+        return False
+
+    def has_child(self, child_name):
+        return child_name in self.children
+
+    def get_child(self, child_name):
+        if self.has_child(child_name):
+            return self.children[child_name]
+        return None
+
+    def __str__(self):
+        return f"{'  ' * self.level}- {self.name} (dir)\n" + \
+    '\n'.join([str(child) for child in self.children.values()]) 
+
+    def is_directory(self):
+        return True
+
+    def visited(self, visitor):
+        for obj in self.children.values():
+            visitor.visit(obj)
+
+
+
 
 class IteratingNode(BaseNode):
     pass
 
 
-
+class Parser:
+    def __init__(self, template):
+        self.template = template
 
 def parse(input, template):
     pass
